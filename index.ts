d217b47c4afc7ccfbfa6af7415f5be438d502ad7
Jamalam360 <56727311+Jamalam360@users.noreply.github.com>
2022-01-05 15:44:55 +0000

    Initial

---
+++ index.ts
@@ -0,0 +1,55 @@
+import { exists } from "https://deno.land/std@0.119.0/fs/mod.ts";
+import { listenAndServe } from "https://deno.land/std@0.119.0/http/server.ts";
+
+interface Config {
+  discord_webhook_url: string;
+  port: number;
+}
+
+async function readConfig(): Promise<Config> {
+  if (await exists("config.json")) {
+    const text = await Deno.readTextFile("config.json");
+    return JSON.parse(text);
+  } else {
+    console.error(
+      "Config file not found! Please create one according to the example.",
+    );
+    Deno.exit(1);
+  }
+}
+
+const config = await readConfig();
+
+const handler = async (req: Request): Promise<Response> => {
+  const json = await req.json();
+
+  if (json.action === "published") {
+    const webhookContent = {
+      embeds: [
+        {
+          title: `New release published`,
+          type: "rich",
+          description:
+            `${json.release.author.login} has published version '${json.release.tag_name}' of ${json.repository.full_name}`,
+          url: json.release.html_url,
+          color: 0x00ff00,
+          fields: [
+            {
+              name: "Release Description",
+              value: json.release.body,
+            },
+          ],
+        },
+      ],
+    };
+
+    await fetch(config.discord_webhook_url, {
+      method: "POST",
+      body: JSON.stringify(webhookContent),
+    });
+  }
+
+  return new Response(null, { status: 200 });
+};
+
+await listenAndServe(`:${config.port}`, handler);
